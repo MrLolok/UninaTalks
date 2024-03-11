fe218771c8befc6de30aec071be92eba2fcfce4e
MrLolok <lolok3221@gmail.com>
2024-03-11 15:27:40 +0100

    - Aggiunta sezione contatti - Aggiunti header delle varie sezioni - Aggiunto footer - Sviluppate le slideshow (stile e funzioni TS)

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,2 +1,43 @@
 /** STYLES **/
 import "../style/style.scss";
+
+/** SLIDESHOW **/
+let slide = 1;
+
+const changeSlideIndex = (index: number): void => {
+    showSlide(slide += index);
+}
+
+const setSlideIndex = (index: number): void => {
+    showSlide(slide = index);
+}
+
+const showSlide = (index: number): void => {
+    const slides: HTMLCollectionOf<Element> = document.getElementsByClassName("slide");
+    const dots: HTMLCollectionOf<Element> = document.getElementsByClassName("dot");
+    if (index > slides.length)
+        slide = 1;
+    else if (index < 1)
+        slide = slides.length;
+
+    for (let i = 0; i < slides.length; i++)
+        (slides[i] as HTMLElement).style.display = "none";
+    for (let i = 0; i < dots.length; i++)
+        dots[i].classList.remove("active");
+    (slides[slide-1] as HTMLElement).style.display = "block";  
+    (dots[slide-1] as HTMLElement).classList.add("active");
+}
+
+/** INITIALIZE AND REGISTER FUNCTIONS */
+declare global {
+    interface Window {
+        changeSlideIndex: any;
+        setSlideIndex: any;
+    }
+}
+
+window.onload = () => {
+    window.changeSlideIndex = changeSlideIndex;
+    window.setSlideIndex = setSlideIndex;
+    showSlide(slide);
+};
